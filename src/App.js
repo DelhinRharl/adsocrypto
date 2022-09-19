8eb56e05319854a51d25461d0cc3172c26cf28ad
DelhinRharl <delhinrharl@gmail.com>
2022-09-19 14:10:59 +0100

    Reconfigure app.js to hold scripts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Landing from './pages/Landing';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <Signup/>
+      <Signin/>
+      <Landing/>
     </div>
   );
 }
