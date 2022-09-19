b57128d3a905b2cd76f2a159cf9f6410632ca659
DelhinRharl <delhinrharl@gmail.com>
2022-09-19 14:23:06 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
@@ -6,6 +7,7 @@ import Signup from './pages/Signup';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Signup/>
       <Signin/>
       <Landing/>
