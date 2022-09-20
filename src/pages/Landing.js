0bdd6eecacf0f92758eb859abe74ab4ee870346a
DelhinRharl <delhinrharl@gmail.com>
2022-09-20 23:16:27 +0100

    edit landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,22 +2,23 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div>
+    <div className="landing">
+      <div className="logo">
+        <img src="" alt="Logo" />
+        </div>
       <h1>
-        Welcome to Adsocrypto
+         adsocrypto
       </h1>
       <div>
-         <section>
-          <div>
-            <p>Begin your journey to the world of cryptocurrencies</p>
-            <input type="text" placeholder="Enter your email address" />
-            <button>Get Started</button>
-          </div>
+         <p>
+            Crypto in your pocket
+         </p>
+      </div>
 
-          <div>
-            <img src="" alt="Chosen Image" />
-          </div>
-      </section>
+      <div className="landing-btn">
+          <button>
+              <a href="Sign Up">Get Started</a>
+          </button>
       </div>
     </div>
 
