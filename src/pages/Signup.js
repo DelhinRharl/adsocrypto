34b4d6987f3ec21b0769b6d2ba7bcf76ddd2faa3
DelhinRharl <delhinrharl@gmail.com>
2022-09-22 07:21:40 +0100

    modify the asterisk

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -8,19 +8,19 @@ const Signup = () => {
           Manage, buy and sell crypto, all in your pocket.
         </p>
         <div className="form">
-            <label htmlFor="firstName">First Name *</label>
+            <label htmlFor="firstName">First Name <span>*</span> </label>
             <input type="text" name="firstName" id="firstName" placeholder="first name" />
 
-            <label htmlFor="lastName">Last Name *</label>
+            <label htmlFor="lastName">Last Name <span>*</span></label>
             <input type="text" name="lastName" id="lastName" placeholder="Last name"/>
 
-            <label htmlFor="email">Email *</label>
+            <label htmlFor="email">Email <span>*</span></label>
             <input type="email" name="email" id="email" placeholder="email"/>
 
-            <label htmlFor="password">Password *</label>
+            <label htmlFor="password">Password <span>*</span></label>
             <input type="password" name="password" id="password" placeholder="password" />
 
-            <label htmlFor="password-confirm">Confirm Password *</label>
+            <label htmlFor="password-confirm">Confirm Password<span>*</span></label>
             <input type="password" name="password-confirm" id="password-confirm" placeholder="confirm password"/>
 
             <button type="submit">Sign Up</button>
