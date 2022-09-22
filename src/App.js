d11ef0894806e74177f2e808492d1622b039b405
DelhinRharl <delhinrharl@gmail.com>
2022-09-22 07:13:16 +0100

    Minor mods of the signup page

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
-      {/* <Signup/> */}
+      <Signup/>
       {/* <Signin/> */}
-      <Landing/>
+      {/* <Landing/> */}
       {/* <Currencies/> */}
       {/* <Footer /> */}
     </div>
