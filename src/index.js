74f28469f72ec39eb7735b60aa559b87beb601cc
DelhinRharl <delhinrharl@gmail.com>
2022-09-29 21:01:00 +0100

    Add provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/main.css';
 import App from './App';
+import {Provider} from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
