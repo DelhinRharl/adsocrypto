82218399d016acfd687e725a47cfad0b12614210
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-11-15 12:58:35 -0800

    Revert "Fetch api"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import Currencies from './pages/Currencies';
 import Landing from './pages/Landing';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
@@ -10,11 +9,10 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Signup/> */}
-      {/* <Signin/> */}
-      {/* <Landing/> */}
-      {/* <Currencies/> */}
-      {/* <Footer /> */}
+      <Signup/>
+      <Signin/>
+      <Landing/>
+      <Footer />
     </div>
   );
 }
